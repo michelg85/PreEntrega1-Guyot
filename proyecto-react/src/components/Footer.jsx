0dc7e82f22c41676c6ceffbb051bed26d100bf12
michelg85 <miguelguyot85@gmail.com>
2022-12-12 14:46:48 -0300

    pre.entrega2

--- proyecto-react/src/components/Footer.jsx
+++ proyecto-react/src/components/Footer.jsx
@@ -7,15 +7,15 @@ const Footer = () => {
             <hr/>
 <div className="row">
     <div className="col-md-6">
-<img src={"images/taiko.png"} alt="Logo Taiko" width="150"/>
+        <img src={"images/taiko.png"} alt="Logo Taiko" width="150"/>
     </div>
-        <div className="col-md-6 text-end">
-            <p>
-            <a href={"https://www.instagram.com/taikosushi.ok/"} target={"_blank"} className="me-3"> <img src={"images/instagram.svg"} alt="Instagram" width="45"/></a>
-    <a href={"https://www.facebook.com/profile.php?id=100075842364275"} target={"_blank"} className="me-3"> <img src={"images/facebook.svg"} alt="Facebook" width="45" /></a>
-            </p>
-   
-</div>
+    <div className="col-md-6 text-end">
+        <p>
+        <a href={"https://www.instagram.com/taikosushi.ok/"} target={"_blank"} className="me-3"> <img src={"images/instagram.svg"} alt="Instagram" width="45"/></a>
+        <a href={"https://www.facebook.com/profile.php?id=100075842364275"} target={"_blank"} className="me-3"> <img src={"images/facebook.svg"} alt="Facebook" width="45"/></a>
+        </p>
+
+    </div>
 </div>
 </div>
 </div>
