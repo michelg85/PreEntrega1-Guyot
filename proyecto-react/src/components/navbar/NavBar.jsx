97dc848474ecedc78f27d43cb183a4c9758174ea
michelg85 <miguelguyot85@gmail.com>
2023-01-04 19:22:59 -0300

    cambios en LOGO

--- proyecto-react/src/components/navbar/NavBar.jsx
+++ proyecto-react/src/components/navbar/NavBar.jsx
@@ -9,26 +9,26 @@ const NavBar = () => {
                 <div className="col-md-6">
                     <nav className="navbar navbar-expand-lg">
                         <div className="container-fluid">
-                            <Link className="navbar-brand" to={"/"}><img src={"images/taiko.png"} alt={"Logo Taiko"} width="150" /></Link>
+                            <Link className="navbar-brand" to={"/"}><img src={"images/taiko.png"} alt={"Logo Taiko"} width="300" /></Link>
                             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                             </button>
                             <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav">
+                            <ul className="navbar-nav navbar-left">
                                     <li className="nav-item">
-                                        <h2><NavLink className="nav-link badge rounded-pill text-bg-dark" aria-current="page" to={"/category/tablas"}>Tablas</NavLink></h2>
+                                        <h4><NavLink className="nav-link text-dark fw-bold" aria-current="page" to={"/category/tablas"}>Tablas</NavLink></h4>
                                     </li>
                                     <li className="nav-item">
-                                    <h2><NavLink className="nav-link badge rounded-pill text-bg-dark" to={"/category/ceviche"}>Ceviches</NavLink></h2>
+                                    <h4><NavLink className="nav-link text-dark fw-bold" to={"/category/ceviche"}>Ceviches</NavLink></h4>
                                     </li>
                                     <li className="nav-item">
-                                    <h2><NavLink className="nav-link badge rounded-pill text-bg-dark" to={"/category/rolls"}>Rolls</NavLink></h2>
+                                    <h4><NavLink className="nav-link text-dark fw-bold" to={"/category/rolls"}>Rolls</NavLink></h4>
                                     </li>
                                     <li className="nav-item">
-                                    <h2><NavLink className="nav-link badge rounded-pill text-bg-dark" to={"/category/wok"}>Woks</NavLink></h2>
+                                    <h4><NavLink className="nav-link text-dark fw-bold" to={"/category/wok"}>Woks</NavLink></h4>
                                     </li>
                                     <li className="nav-item">
-                                    <h2><NavLink className="nav-link badge rounded-pill text-bg-dark" to={"/category/entradas"}>Entradas</NavLink></h2>
+                                    <h4><NavLink className="nav-link text-dark fw-bold" to={"/category/entradas"}>Entradas</NavLink></h4>
                                     </li>
                                 </ul>
 
@@ -36,11 +36,14 @@ const NavBar = () => {
                         </div>
                     </nav>
                 </div>
-                <div className="col-md-6 d-flex justify-content-end align-items-center">
+                <div className="col-md-6 d-flex justify-cr align-items-center">
+            
                     <CartWidget/>
                 </div>
             </div>
+            
         </div>
+        
     )
 }
 
