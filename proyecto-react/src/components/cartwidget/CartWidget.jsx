ac1632ab73e8dbc2b28a03e8a74d592692c7e5d6
michelg85 <miguelguyot85@gmail.com>
2023-01-16 14:05:26 -0300

    cambios en CartContext

--- proyecto-react/src/components/cartwidget/CartWidget.jsx
+++ proyecto-react/src/components/cartwidget/CartWidget.jsx
@@ -5,14 +5,16 @@ import { Link } from "react-router-dom";
 const CartWidget = () => {
     const {cartTotal} = useContext(CartContext);
 
-    return cartTotal() ? <Link to={"/cart"} className="btn position-relative" title="Ir al Carrito">
+    return  (
+<Link to={"/cart"} className="btn position-relative" title="Ir al Carrito">
     <img src={"../images/cart3.svg"} alt={"Carrito"} width={24} />
-    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{cartTotal()}</span>
-</Link> : "";
+   {cartTotal() > 0 && <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{cartTotal()}</span>}
+</Link> 
+)
     
 }
 
-export default CartWidget;
+export default CartWidget;
 
 
 
